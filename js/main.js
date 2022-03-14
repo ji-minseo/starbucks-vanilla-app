eef12b18e9937e71eba8599d5b0ad577dd442768
서지민 <seojimin@seojimicBookAir.lan>
2022-03-14 10:27:37 +0900

    swiperjs 토글, 화살표 만들기

--- js/main.js
+++ js/main.js
@@ -57,3 +57,22 @@ new Swiper('.notice-line .swiper-container', {
     autoplay: true,
     loop: true
 });
+
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination',//페이지번호 요소 선택자
+        clickable: true //사용자의 페이지 번호 요소 제어 가능여부
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
